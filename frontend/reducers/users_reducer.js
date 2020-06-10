05fb5e6355efffcc03db92bfe9ee0e3706e1fe31
jpedrosiq <johnpedrosiqueira@gmail.com>
2020-06-10 15:54:27 -0700

    fix reducer

--- frontend/reducers/users_reducer.js
+++ frontend/reducers/users_reducer.js
@@ -8,14 +8,22 @@ const usersReducer = (state = {}, action) => {
         case RECEIVE_CURRENT_USER:
             return Object.assign({}, state, { [action.currentUser.id]: action.currentUser })
         case RECEIVE_MY_LIST_ITEMS:
+    // Another way?
+            // return Object.assign({}, state, action.videos.listVideoIds);
+
+    // My Way    
             let newState = Object.assign({}, state);
             newState[Object.keys(newState)[0]].listVideoIds = action.videos.listVideoIds;
+            return newState;
+
+
+    // Helen's way:     
             // for (let key in newState) {
             //     if (!action.videos.listVideoIds.includes(parseInt(key))) {
             //         delete newState[key];
             //     }
             // }
-            return newState;
+            // return newState;
         default:
             return state;
     }
